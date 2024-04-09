e353f29c0e4c57cf085bf1ee73d28e53502de949
Nil E <nil.erdenn@gmail.com>
2024-04-09 16:46:56 +0200

    finished 4 iterations

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 
 const celebritySchema = new Schema(
-  {name: String,
+  { name: String,
     occupation: String,
     catchPhrase: String
   }
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,6 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
 
 // all your routes here
 router.get("/celebrities/create", (req, res, next) => {
@@ -7,25 +8,26 @@ router.get("/celebrities/create", (req, res, next) => {
 });
 
 router.post("/celebrities/create", (req, res, next) => {
-   return celebrities.create(req.body)
+   return Celebrity.create(req.body)
     .then(() => {
-        res.render("celebrities/celebrities");
+      return Celebrity.find()
+        .then((celebs) => {
+        res.render("celebrities/celebrities",{celebs});})
     })
     .catch((error) => {
+      console.log("error",error);
         res.render("celebrities/new-celebrity");
     });
 });
 
-router.get("/celebrities", (req, res, next) => {
-    Celebrity.find()
-    .then(() => {
-        
+router.get("/celebrities/celebrities", (req, res, next) => {
+    return Celebrity.find()
+    .then((celebs) => {
+      res.render("celebrities/celebrities",{celebs});
     })
     .catch((error) => {
         console.log("error",error);
     });
-
-    res.render('celebrities/celebrities');
   });
 
 
